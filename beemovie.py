080cdf97d7cd2621da7aae8720a7967268a13e79
unknown <snake09031999@gmail.com>
2020-11-19 15:29:07 +0100

    add comment

--- beemovie.py
+++ beemovie.py
@@ -2,6 +2,8 @@ import pyautogui, time
 time.sleep(2)  
 f = open("beemovie.txt", 'r')
 for word in f:
+    # you can add a timer there to control timing of the spam. 
+    # time.sleep(1)    #will trigger every second 
     pyautogui.typewrite(word)
     pyautogui.press("enter")
     
